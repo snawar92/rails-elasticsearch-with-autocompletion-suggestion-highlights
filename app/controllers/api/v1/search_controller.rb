93413208834e59083fe81f1f35537591b9a3e9f3
snawar92 <snawar92@gmail.com>
2020-06-09 21:21:29 -0400

    if the search turns up no results, we turn to the autocompletor

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::SearchController < ApplicationController
   def create
-    @posts = params["query"].nil? ? [] : Movie.search(params["query"])
-    render json: @posts
+    @movies = params["query"].nil? ? [] : Movie.search(params["query"])
+    if @movies.results.total > 0
+      render json: @movies
+    else
+      render json: Autocompleter.call(
+        params["query"], 
+        {
+          model: Movie,
+          fields: ['name', 'description'],
+          hint_field: 'name'
+        }     
+      )
+    end
   end
 end
