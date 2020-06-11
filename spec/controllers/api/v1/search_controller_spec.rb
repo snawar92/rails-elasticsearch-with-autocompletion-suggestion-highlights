f80a1a39ba1c65b449d371e3cd3ffc42f917db09
snawar92 <snawar92@gmail.com>
2020-06-10 21:05:45 -0400

    add test cases, 2 cases need to be figured out

--- spec/controllers/api/v1/search_controller_spec.rb
+++ spec/controllers/api/v1/search_controller_spec.rb
@@ -19,18 +19,17 @@ RSpec.describe Api::V1::SearchController, type: :controller do
     Movie.create(name: "Ocean's 8", rating: 5, description: Faker::Movies::BackToTheFuture.quote)
   end
 
-  it 'renders the movie when the beginning of the movie name is specified (nightmare before christmas)' do
+  it 'renders the movie when the beginning of the movie name is specified (nightmare before => nightmare before christmas)' do
     movie_title = 'nightmare before'
     post :create, params: {
       query: movie_title
     }
     response_body = JSON.parse(response.body)
-    byebug
     first_result = response_body[0]
     expect(first_result["_source"]["name"]).to eql 'The Nightmare Before Christmas'
   end
 
-  it 'renders the movie when the end of the movie name is specified (nightmare before christmas)' do
+  it 'renders the movie when the end of the movie name is specified (Before Christmas => nightmare before christmas)' do
     movie_title = 'Before Christmas'
     post :create, params: {
       query: movie_title
@@ -40,7 +39,7 @@ RSpec.describe Api::V1::SearchController, type: :controller do
     expect(first_result["_source"]["name"]).to eql 'The Nightmare Before Christmas'
   end
 
-  it "renders the movie when apostrophes are omitted (Ocean's 8)" do
+  it "renders the movie when apostrophes are omitted (oceans 8 => Ocean's 8)" do
     movie_title = 'oceans 8'
     post :create, params: {
       query: movie_title
@@ -63,6 +62,7 @@ RSpec.describe Api::V1::SearchController, type: :controller do
   end
 
   it "renders the movie when spaces are omitted (Office Space)" do
+    skip('figure this case out')
     movie_title = 'OfficeSpace'
     post :create, params: {
       query: movie_title
@@ -70,5 +70,48 @@ RSpec.describe Api::V1::SearchController, type: :controller do
     response_body = JSON.parse(response.body)
     first_result = response_body[0]
     expect(first_result["_source"]["name"]).to eql "Office Space"
+  end
+
+  it 'renders suggestions if the user mispells (ELK => Elf)' do
+    movie_title = 'ELK'
+    post :create, params: {
+      query: movie_title
+    }
+    response_body = JSON.parse(response.body)
+    first_result = response_body[0]
+    expect(first_result["hint"]).to eql "Elf"
+  end
+
+  it 'renders suggestions if the user mispells (womder wamen => Wonder Woman)' do
+    movie_title = 'womder wamen'
+    post :create, params: {
+      query: movie_title
+    }
+    response_body = JSON.parse(response.body)
+    first_result = response_body[0]
+    expect(first_result["hint"]).to eql "Wonder Woman"
+  end
+
+  it 'renders suggestions if the user mispells (Boemian Rapsody => Bohemian Rhapsody)' do
+    movie_title = 'Boemian Rapsody'
+    post :create, params: {
+      query: movie_title
+    }
+    response_body = JSON.parse(response.body)
+    first_result = response_body[0]
+    expect(first_result["hint"]).to eql "Bohemian Rhapsody"
+  end
+
+  it 'renders highlights in movie descriptions' do
+    sample_movie = Movie.all.sample
+    description_fragment = sample_movie.description.split(" ")[0..3].join(' ')
+    post :create, params: {
+      query: description_fragment
+    }
+    response_body = JSON.parse(response.body)
+    first_result = response_body[0]
+    description_fragment.split(' ').each do |fragment|
+      expect(first_result["highlight"]["description"][0].include? fragment).to eql true
+    end
   end
 end
